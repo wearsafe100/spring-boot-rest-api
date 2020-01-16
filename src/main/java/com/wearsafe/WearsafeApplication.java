bc9076521195ff7b44313989d1e374cc9d5a2a0a
NAGENDRA YADAV <58745011+wearsafe100@users.noreply.github.com>
2020-01-16 16:43:22 -0500

    Revert "new file is created!"

--- src/main/java/com/wearsafe/WearsafeApplication.java
+++ src/main/java/com/wearsafe/WearsafeApplication.java
@@ -3,14 +3,11 @@ package com.wearsafe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.wearsafe.web.controller.testDeadlockExample;
-
 @SpringBootApplication
 public class WearsafeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WearsafeApplication.class, args);
-	    new testDeadlockExample().main(new String[] {});
 	}
 
 }
